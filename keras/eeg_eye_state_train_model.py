e6efe19cfa04477b5e117f83cb3e747334e5c1d1
Vitaliy Brydinskiy <fartenko@gmail.com>
2017-10-19 11:01:02 +0300

    EEG Train model sceleton

--- keras/eeg_eye_state_train_model.py
+++ keras/eeg_eye_state_train_model.py
@@ -1,13 +1,30 @@
-# Here will be an EEG Eye State model training
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as k
 
-import math as m
+# TODO: Load EEG dataset
 
+batch_size = 128
+num_classes = 2
+epochs = 12
 
-print(m.sin(3.1415))
+# TODO: Define input data variable
+# x_train = load data from dataset
+# same for output
 
-for x in range(1, 10):
-    print(x)
+# TODO: Define types of input data
+# var.astype( ... )
 
+# creating model
+model = Sequential()
+# TODO: Add appropriate model layers here! (model.add)
 
-for i in range(1, 2):
-    pass
+# TODO: Compile model (model.compile)
+
+# TODO: Train model (model.fit)
+
+# TODO: Evaluate model (model.evaluate)
+
+model.save('models/keras_eeg_eye.h5')
